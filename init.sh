bd43cc66f87a991a5cb59a092b691623be2bd3c7
motojouya <motojouya@gmail.com>
2019-08-19 07:01:48 +0900

    change from ec2

--- init.sh
+++ init.sh
@@ -2,63 +2,33 @@
 set -x
 
 # definitions
-export AWS_DEFAULT_REGION=asia-northeast1
+# export AWS_DEFAULT_REGION=asia-northeast1
 # asia-northeast1-b
 
-region=$1
-userid=$2
-username=$3
-password=$4
-ssh_port=$5
-hosted_zone_id=$6
-domain=$7
-volume_id=$8
+username=$1
+ssh_port=$2
+hosted_zone_id=$2
+domain=$4
 
-instance_id=$(curl -s 169.254.169.254/latest/meta-data/instance-id)
 ip=$(curl -s 169.254.169.254/latest/meta-data/public-ipv4)
 
 cd /home/ubuntu
 
-# install awscli
-cp -p /etc/apt/sources.list /etc/apt/sources.list.bak
-sed -i 's/ap-northeast-1\.ec2\.//g' /etc/apt/sources.list
-apt update
-apt install nvme-cli
-apt install -y unzip
-apt install -y python
-# DEBIAN_FRONTEND=noninteractive dpkg --configure -a --force-confdef --force-confnew
-# apt install -y python3-pip
-# pip3 install awscli
-if ! test -e /usr/bin/aws ; then
-  curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
-  unzip /tmp/awscli-bundle.zip -d /tmp
-  /tmp/awscli-bundle/install -i /usr/lib/aws -b /usr/bin/aws
-fi
+apt-get update
+apt-get install nvme-cli
 
-# mount ebs volume
-aws ec2 attach-volume --volume-id vol-$volume_id --instance-id $instance_id --device /dev/xvdb --region $region
-aws ec2 wait volume-in-use --volume-ids vol-$volume_id
-device=$(nvme list | grep $volume_id | awk '{print $1}' | xargs)
-while [ -z $device ]; do
-    sleep 1
-    device=$(nvme list | grep $volume_id | awk '{print $1}' | xargs)
-done
-# until [ -e $device ]; do
-#     sleep 1
-# done
-mkdir /home/$username
-# mkfs -t ext4 $device
-mount $device /home/$username
+# gcloud
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y google-cloud-sdk
 
-# add user
-useradd -u $userid -d /home/$username -s /bin/bash $username
-gpasswd -a $username sudo
-cp -arpf /home/ubuntu/.ssh/authorized_keys /home/$username/.ssh/authorized_keys
-chown $username /home/$username
-chgrp $username /home/$username
-chown -R $username /home/$username/.ssh
-chgrp -R $username /home/$username/.ssh
-echo "$username:$password" | chpasswd
+# mount disk
+mkdir /home/$username/work
+mount $device /home/$username/work
+ln -s /home/$username/work/dev ./dev
+ln -s /home/$username/work/doc ./doc
 
 # register route53
 curl https://raw.githubusercontent.com/motojouya/ec2-develop/master/dyndns.tmpl -O
@@ -72,19 +42,19 @@ cp sshd_config.init /etc/ssh/sshd_config
 systemctl restart sshd
 
 # install nodejs
-apt install -y nodejs
-apt install -y npm
+apt-get install -y nodejs
+apt-get install -y npm
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt update
-apt install -y yarn
+apt-get update
+apt-get install -y yarn
 npm install -g npx
 npm install -g typescript typescript-language-server
 yarn global add create-react-app
 
 # install docker
-sudo apt install -y \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -111,27 +81,24 @@ cp /home/$username/letsencrypt.tar.gz letsencrypt.tar.gz
 tar xzf letsencrypt.tar.gz
 cd /home/ubuntu
 
-apt install -y nginx
+apt-get install -y nginx
 curl https://raw.githubusercontent.com/motojouya/ec2-develop/master/http.conf.tmpl -O
 sed -e s/{%domain%}/$domain/g http.conf.tmpl > http.conf
 cp http.conf /etc/nginx/conf.d/http.conf
 
-apt install -y software-properties-common
+apt-get install -y software-properties-common
 add-apt-repository -y universe
 add-apt-repository -y ppa:certbot/certbot
-apt update
-apt install -y certbot python-certbot-nginx
+apt-get update
+apt-get install -y certbot python-certbot-nginx
 
 # install library for puppeteer
-# apt install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+# apt-get install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 
 # install others
-apt install -y neovim
-apt install -y jq
-apt install -y tree
+apt-get install -y neovim
+apt-get install -y jq
+apt-get install -y tree
 
 # git config --global core.editor 'vim -c "set fenc=utf-8"'
-
-cd /
-userdel -r ubuntu
 
